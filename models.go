7a161fbfcb67c38a26a7382278e3f6bd95d4f3cb
Spencer Geoffrey Edgecombe <edge1190@Spencers-MacBook-Pro.local>
2018-11-13 10:35:41 -0500

    add data types

---
+++ models.go
@@ -0,0 +1,38 @@
+package freeze_pos_api
+
+type MenuItem struct {
+	ID int `json:"id,omitempty"`
+	Title string `json:"title,omitempty"`
+	Price float32 `json:"price,omitempty"`
+	Ingredients []int `json:"ingredients,omitempty"`
+	Preparations []int `json:"preparations,omitempty"`
+	Groups []int `json:"groups,omitempty"`
+}
+
+type Ingredient struct {
+	ID int `json:"id,omitempty"`
+	Title string `json:"title,omitempty"`
+	Price float32 `json:"price,omitempty"`
+}
+
+type Preparation struct {
+	ID int `json:"id,omitempty"`
+	Title string `json:"title,omitempty"`
+}
+
+type Group struct {
+	ID int `json:"id,omitempty"`
+	Title string `json:"title,omitempty"`
+	Ingredients []int `json:"ingredients,omitempty"`
+	Preparations []int `json:"preparations,omitempty"`
+}
+
+type Menu struct {
+	Ingredients []Ingredient `json:"ingredients,omitempty"`
+	MenuItems []MenuItem `json:"menu_items,omitempty"`
+	Preparations []Preparation `json:"preparations,omitempty"`
+	Groups []Group `json:"groups,omitempty"`
+}
+
+
+
